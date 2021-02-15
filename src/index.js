2985e5df871c48e95da4eed4bd8a57ae83beb42b
Metamorg <egoronokasd@gmail.com>
2021-02-15 22:55:48 +0300

    feat: implement reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+     let arr = Math.abs(n).toString().split('');
+     for (var i = 0; i < arr.length - 1; i++) {
+        arr.splice(i, 0, arr.pop());
+    }
+    return Number(arr.join(''));
 }
+
